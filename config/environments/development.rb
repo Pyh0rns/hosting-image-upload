3b87f5509095fd98205867edfa7747e9cc5992ec
PierreYves Hoorens <pyhoorens@gmail.com>
2023-02-24 10:15:02 +0100

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # Show full error reports.
+  # Show full error reports.config.active_storage.service = :cloudinary
   config.consider_all_requests_local = true
 
   # Enable server timing
@@ -34,7 +34,9 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
+
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
